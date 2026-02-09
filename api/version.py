d026f765ce93b0c2e767dbc5a646f26395c6fdab
Azerxim <mallet.maxime@hotmail.fr>
2026-02-09 20:51:05 +0100

    Mise à jour du suffixe de version pour la build nocturne

--- api/version.py
+++ api/version.py
@@ -5,7 +5,7 @@ _MINOR = "0"
 _PATCH = "1"
 # This is mainly for nightly builds which have the suffix ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
-_SUFFIX = ".20260119.1"
+_SUFFIX = ".20260209.1"
 
 __version__ = f'{_MAJOR}.{_MINOR}.{_PATCH}'
 __version_dev__ = f'{__version__}{_SUFFIX}'
